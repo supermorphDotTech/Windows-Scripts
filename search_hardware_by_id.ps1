6dd4b3754065d40fda77dcd3f6e285cd55dbaf1e
supermorph.tech <148707180+supermorphDotTech@users.noreply.github.com>
2024-04-03 12:04:30 +0200

    Update search_hardware_by_id.ps1
    Print to console optimized.

--- search_hardware_by_id.ps1
+++ search_hardware_by_id.ps1
@@ -6,7 +6,7 @@
 # for instance the event viewer references a device error, it is usually not clear which
 # device that would refer to. This script however, will make it clear.
 
-# The search uses likeness, which means you can also just use for a snippet of the
+# The search uses likeness, which means you can also just search for a snippet of the
 # hardware ID (i.e. PCI\VEN_1022).
 
 # **************************************** #
@@ -26,7 +26,7 @@ $deviceDetected = $false
 $enum = 0
 
 # Define device search
-$deviceList = Get-PnpDevice | Select-Object -Property FriendlyName, DeviceID
+$deviceList = Get-PnpDevice | Select-Object -Property Status, Class, FriendlyName, DeviceID
 
 # Loop through the registry entries.
 foreach($device in $deviceList) {
@@ -37,9 +37,16 @@ foreach($device in $deviceList) {
 	if ($checkID -like "*$($pciId)*") {
 		
 		$enum++
+		
+		if ($enum -eq 1) {
+		Write-Output "`n[NR]..[STATUS]..[CLASS]..[NAME]..[ID]`n"
+		}
+		
 		$deviceDetected = $true
 		$deviceName = $device.FriendlyName
-		Write-Output "[$($enum)] $($deviceName) - $($checkID)"
+		$deviceStatus = $device.Status
+		$deviceClass = $device.Class
+		Write-Output "[$($enum)] [$($deviceStatus)] $($deviceClass) | $($deviceName) | $($checkID)"
 	}
 }
 
@@ -49,12 +56,14 @@ if($deviceDetected -eq $false) {
 	Write-Output "No device is matching $($pciId)"
 	
 	# Offer a grid view of hardware devices to be displayed.
-	$displayGridResponse = Read-Host "Do you want to display device grid view? [(y)es / (n)o]"
+	# $displayGridResponse = Read-Host "Do you want to display device grid view? [(y)es / (n)o]"
 	
-	if($displayGridResponse -eq "y" -or $displayGridResponse -eq "yes") {
-		Get-PnPDevice | Out-GridView -Title 'Select a device' -OutputMode Single | Select-Object -Property *
-	}
+	# if($displayGridResponse -eq "y" -or $displayGridResponse -eq "yes") {
+		# Get-PnPDevice | Out-GridView -Title 'Select a device' -OutputMode Single | Select-Object -Property *
+	# }
 }
 
+Write-Output ""
+
 # Wait for user to exit script.
-Read-Host -Prompt "Press Enter to exit"
+# Read-Host -Prompt "Press Enter to exit"
